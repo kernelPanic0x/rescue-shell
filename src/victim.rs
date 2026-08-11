d3de3c04ecfeb8488c603d3e9686b3a6b7ed9350
Elias Dalbeck <eliasdalbeck42@gmail.com>
2026-08-11 08:33:55 +0200

    added RESCUE_SHELL env var to find elf

--- src/victim.rs
+++ src/victim.rs
@@ -86,6 +86,7 @@ impl PtySession {
         let term = std::env::var("TERM").unwrap_or_else(|_| "xterm-256color".to_string());
         let mut cmd = CommandBuilder::new(&shell);
         cmd.env("TERM", term);
+        cmd.env("RESCUE_SHELL", std::env::current_exe()?);
         let mut child = pair
             .slave
             .spawn_command(cmd)
